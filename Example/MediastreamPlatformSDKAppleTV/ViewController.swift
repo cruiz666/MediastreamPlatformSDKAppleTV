879ec781dcf05a3b3dbc1171c7e13bbb14f36036
Carlos Ruiz <cruiz@mediastre.am>
2022-01-05 12:27:34 -0300

    Initial commit

---
+++ Example/MediastreamPlatformSDKAppleTV/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MediastreamPlatformSDKAppleTV
+//
+//  Created by Carlos Ruiz on 01/05/2022.
+//  Copyright (c) 2022 Carlos Ruiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
